258720152bd50e0c38ea642a95227e16a401c8f6
SKREAMMY <kiran.pillai199@gmail.com>
2025-01-06 13:17:16 +0000

    UI changes in Link page and footer

--- src/pages/Links/LinksPage.jsx
+++ src/pages/Links/LinksPage.jsx
@@ -2,108 +2,61 @@ import React from "react";
 import "./LinksPage.css";
 
 const LinksPage = () => {
+  const links = [
+    {
+      icon: "fa-trash",
+      title: "Bin Collection Dates",
+      url: "https://www.cambridge.gov.uk/check-when-your-bin-will-be-emptied",
+    },
+    {
+      icon: "fa-file",
+      title: "Council Tax Account and Bill",
+      url: "https://www.cambridge.gov.uk/manage-your-council-tax-account-and-check-your-bill",
+    },
+    {
+      icon: "fa-car",
+      title: "Parking",
+      url: "https://www.cambridge.gov.uk/parking",
+    },
+    {
+      icon: "fa-flag",
+      title: "Report Issues",
+      url: "https://www.cambridge.gov.uk/report-it",
+    },
+    {
+      icon: "fa-money",
+      title: "Pay",
+      url: "https://ip.e-paycapita.com/AIP/itemSelectionPage.do?link=showItemSelectionPage&siteId=85&languageCode=EN&source=AIP",
+    },
+    {
+      icon: "fa-list",
+      title: "Check Planning Applications",
+      url: "https://applications.greatercambridgeplanning.org/online-applications/",
+    },
+  ];
+
   return (
-    <>
-      <div className="row linkpage">
-        <div className="col-lg-3 col-md-4 col-sm-6 ">
-          <div className="container">
-            <div className="card bin-card">
-              <div className="card-body d-flex flex-column bin-body">
-                <i className="fa fa-trash fa-bounce icon"></i>
-                {/* <h4>Bin Collection Dates</h4> */}
+    <div className="container LinksPage">
+      <div className="row g-4">
+        {links.map((link, index) => (
+          <div className="col-lg-4 col-md-6 col-sm-12" key={index}>
+            <div className="card linkCard">
+              <div className="card-body text-center">
+                <i className={`fa ${link.icon} icon`}></i>
                 <a
-                  className="linkName"
+                  href={link.url}
                   target="_blank"
-                  href="https://www.cambridge.gov.uk/check-when-your-bin-will-be-emptied"
+                  rel="noopener noreferrer"
+                  className="linkName"
                 >
-                  <h4>Bin Collection dates</h4>
+                  <h4>{link.title}</h4>
                 </a>
               </div>
             </div>
           </div>
-        </div>
-        <div className="col-lg-3 col-md-4 col-sm-6">
-          <div className="container">
-            <div className="card bin-card ">
-              <div className="card-body d-flex flex-column bin-body">
-                <i className="fa fa-file icon"></i>
-                <a
-                  className="linkName"
-                  target="_blank"
-                  href="https://www.cambridge.gov.uk/manage-your-council-tax-account-and-check-your-bill"
-                >
-                  <h4>Council Tax account and Bill</h4>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-4 col-sm-6">
-          <div className="container">
-            <div className="card bin-card ">
-              <div className="card-body d-flex flex-column bin-body">
-                <i className="fa fa-car icon"></i>
-                <a
-                  className="linkName"
-                  target="_blank"
-                  href="https://www.cambridge.gov.uk/parking"
-                >
-                  <h4>Parking</h4>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-4 col-sm-6">
-          <div className="container">
-            <div className="card bin-card ">
-              <div className="card-body d-flex flex-column bin-body">
-                <i className="fa fa-flag icon"></i>
-                <a
-                  className="linkName"
-                  target="_blank"
-                  href="https://www.cambridge.gov.uk/report-it"
-                >
-                  <h4>Report issues</h4>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-4 col-sm-6">
-          <div className="container">
-            <div className="card bin-card ">
-              <div className="card-body d-flex flex-column bin-body">
-                <i className="fa fa-money icon"></i>
-                <a
-                  className="linkName"
-                  target="_blank"
-                  href="https://ip.e-paycapita.com/AIP/itemSelectionPage.do?link=showItemSelectionPage&siteId=85&languageCode=EN&source=AIP"
-                >
-                  <h4>Pay</h4>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-4 col-sm-6">
-          <div className="container">
-            <div className="card bin-card ">
-              <div className="card-body d-flex flex-column bin-body">
-                <i className="fa fa-list icon"></i>
-                <a
-                  className="linkName"
-                  target="_blank"
-                  href="https://applications.greatercambridgeplanning.org/online-applications/"
-                >
-                  <h4>Check Planning Applications</h4>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
